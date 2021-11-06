b7510ab0dec96d482256570916d0f754445e1d68
cola <jn123977@163.com>
2021-11-06 15:08:42 +0800

    更改

--- src/main/java/com/cola/pojo/ProjectType.java
+++ src/main/java/com/cola/pojo/ProjectType.java
@@ -4,6 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+/**
+ * @author cola
+ * @version 1.0
+ * @date 2021/10/5 下午7:19
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
